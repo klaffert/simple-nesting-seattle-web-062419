0633ee98e162b5e46d3991b2f8f0865a86b3353d
Kelliann Lafferty <kell10192@gmail.com>
2019-06-11 04:03:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
-     .to_s 
+     programmer_hash[:dennis_ritchie][:languages] = to_s
+     
 end
 
 def adding_matz
