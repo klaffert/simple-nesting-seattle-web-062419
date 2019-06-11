2744b51331462271824f44ef4787dc6dba4b873e
Kelliann Lafferty <kell10192@gmail.com>
2019-06-11 04:28:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,6 +119,6 @@ def adding_to_dennis
         }
      }
      new_language = "Assembly"
-     programmer_hash[:dennis_ritchie][:languages] = new_language
+     programmer_hash[:dennis_ritchie][:languages] << new_language
      programmer_hash
 end
