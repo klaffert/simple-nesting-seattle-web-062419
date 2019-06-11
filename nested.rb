22c3bf7ec018077ef2366328e9a7fedbad94288b
Kelliann Lafferty <kell10192@gmail.com>
2019-06-11 04:07:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,15 +60,9 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
- 		{
- 		  :yukihiro_matsumoto => {
+	{
+ 		  :yukihiro_matsumoto => {}
  		  :known_for => "Ruby",
  		  :languages => ["LISP", "C"]
     }
@@ -85,8 +79,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
 end
 
 def changing_alan
