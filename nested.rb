8410de60298f30c140fc3c1e75204bd5f4eeb020
Kelliann Lafferty <kell10192@gmail.com>
2019-06-11 04:17:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -98,7 +98,8 @@ def changing_alan
         }
      }
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = ""
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     puts programmer_hash
      
 end
 
